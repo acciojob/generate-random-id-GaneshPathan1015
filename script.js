4138eb981ad6cc8a0357af66f1e9122f951e5898
Ganesh Pathan <118661019+GaneshPathan1015@users.noreply.github.com>
2023-12-15 11:55:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
+
+
+
 function makeid(l) {
   // write your code here
+	let str ="";
+	for(let i=0;i<l;i++){
+		let rand = generat();
+		str =str+String.fromCharCode(rand);
+	}
+	return str;
+
+	function generat() {
+		let gen = parseInt(Math.random()*100);
+		if((gen>=48 && gen<=57) || (gen>=65 && gen<=90) || (gen>=97 && gen<=100)){
+			return gen;
+		}
+		else{
+			return 100 + parseInt(gen/5);
+		}
+    }
 }
 
 // Do not change the code below.
